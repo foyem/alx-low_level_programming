0e66d8faeb2e9270db3a6bd23fb92781eb91e470
foyem <anifowose.adeyemi@gmail.com>
2022-04-02 04:28:06 +0100

    new file

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main- This function display the words given.
+(* a blank line
+ * Description: I used main to display _putchar
+(* section header: Section description)*
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	/*this spell the _putchar*/
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
