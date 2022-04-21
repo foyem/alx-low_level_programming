758af493046d11cd78bca36af2ef9dfcff88db6a
foyem <anifowose.adeyemi@gmail.com>
2022-04-21 20:19:04 +0100

    new changes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _LISTS_
+#define _LISTS_
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -24,6 +27,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-
 #endif
-
